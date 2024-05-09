7e22ecd0a5e50aff8175f61f90c297e15315551c
Shreyas Sudarsan <61539685+shreggit@users.noreply.github.com>
2024-05-08 23:48:50 -0400

    Add files via upload

--- Imitation Learning Final/main.py
+++ Imitation Learning Final/main.py
@@ -1,34 +1,38 @@
-from stable_baselines3 import PPO  # pip install stable-baselines3
-
-from luxai2021.env.agent import AgentFromStdInOut
-from luxai2021.env.lux_env import LuxEnvironment
-from luxai2021.game.constants import LuxMatchConfigs_Default
-from agent_policy import AgentPolicy
-
+from typing import Dict
+import sys
+from agent import agent
+import multiprocessing
 if __name__ == "__main__":
-    """
-    This is a kaggle submission, so we don't use command-line args
-    and assume the model is in model.zip in the current folder.
-    """
-    # Tool to run this against itself locally:
-    # "lux-ai-2021 --seed=100 main_lux-ai-2021.py main_lux-ai-2021.py --maxtime 10000"
-
-    # Run a kaggle submission with the specified model
-    configs = LuxMatchConfigs_Default
-
-    # Load the saved model
-    #model_id = 5403
-    #total_steps = int(48e6)
-    #model = PPO.load(f"models/rl_model_{model_id}_{total_steps}_steps.zip")
-    model = PPO.load(f"rl_model_1_12000000_steps.zip")
-    
-    # Create a kaggle-remote opponent agent
-    opponent = AgentFromStdInOut()
-
-    # Create a RL agent in inference mode
-    player = AgentPolicy(mode="inference", model=model)
-
-    # Run the environment
-    env = LuxEnvironment(configs, player, opponent)
-    env.reset()  # This will automatically run the game since there is
-    # no controlling learning agent.
+    multiprocessing.freeze_support()
+    def read_input():
+        """
+        Reads input from stdin
+        """
+        try:
+            return input()
+        except EOFError as eof:
+            raise SystemExit(eof)
+    step = 0
+    class Observation(Dict[str, any]):
+        def __init__(self, player=0) -> None:
+            self.player = player
+            # self.updates = []
+            # self.step = 0
+    observation = Observation()
+    observation["updates"] = []
+    observation["step"] = 0
+    player_id = 0
+    while True:
+        inputs = read_input()
+        observation["updates"].append(inputs)
+        
+        if step == 0:
+            player_id = int(observation["updates"][0])
+            observation.player = player_id
+        if inputs == "D_DONE":
+            actions = agent(observation, None)
+            observation["updates"] = []
+            step += 1
+            observation["step"] = step
+            print(",".join(actions))
+            print("D_FINISH")
